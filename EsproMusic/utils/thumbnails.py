31f0c9a4fd32d39674323bf9ee3908e7e91c1071
𝖭ᴏᴛ 𝖢σᴅєʀ <shamansh3137@gmail.com>
2025-12-20 21:20:41 +0530

    ᴘᴜʀᴠɪ ʙᴏᴛs !!

--- EsproMusic/utils/thumbnails.py
+++ EsproMusic/utils/thumbnails.py
@@ -5,7 +5,7 @@ import aiofiles
 import aiohttp
 from PIL import Image, ImageDraw, ImageEnhance, ImageFilter, ImageFont
 from unidecode import unidecode
-from youtubesearchpython.__future__ import VideosSearch
+from py_yt import VideosSearch
 
 from EsproMusic import app
 from config import YOUTUBE_IMG_URL
