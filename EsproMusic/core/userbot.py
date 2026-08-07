9c9998fd3789433ef180b41ba7e51adbf6c04606
ruchi5134 <kn043613@gmail.com>
2026-08-07 13:26:26 +0530

    Change chat joins to 'Ritik_Chatting_Zone'

--- EsproMusic/core/userbot.py
+++ EsproMusic/core/userbot.py
@@ -51,8 +51,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("EsproSupport")
-                await self.one.join_chat("EsproUpdate")
+                await self.one.join_chat("Ritik_Chatting_Zone")
+                await self.one.join_chat("Ritik_Chatting_Zone")
             except:
                 pass
             assistants.append(1)
@@ -72,8 +72,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("EsproSupport")
-                await self.two.join_chat("EsproUpdate")
+                await self.two.join_chat("Ritik_Chatting_Zone")
+                await self.two.join_chat("Ritik_Chatting_Zone")
             except:
                 pass
             assistants.append(2)
@@ -93,8 +93,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("EsproSupport")
-                await self.three.join_chat("EsproUpdate")
+                await self.three.join_chat("Ritik_Chatting_Zone")
+                await self.three.join_chat("Ritik_Chatting_Zone")
             except:
                 pass
             assistants.append(3)
@@ -114,8 +114,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("EsproSupport")
-                await self.four.join_chat("EsproUpdate")
+                await self.four.join_chat("Ritik_Chatting_Zone")
+                await self.four.join_chat("Ritik_Chatting_Zone")
             except:
                 pass
             assistants.append(4)
@@ -135,8 +135,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("EsproSupport")
-                await self.five.join_chat("EsproUpdate")
+                await self.five.join_chat("Ritik_Chatting_Zone")
+                await self.five.join_chat("Ritik_Chatting_Zone")
             except:
                 pass
             assistants.append(5)
